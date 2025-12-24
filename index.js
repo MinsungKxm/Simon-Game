b55295f5966214612dc99b155df45fa1a2c67589
Minsung Kim <minsungk101@gmail.com>
2025-12-24 12:31:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var gamePattern = []
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonArray[randomNumber];
-  gamePattern.append(randomChosenColor);
-  $("button #" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio(url("./sounds/" + buttonArray[randomChosenColor] + ".mp3"));
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
   audio.play();
 }
