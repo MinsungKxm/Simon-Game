7b8cb0687e2e8ebcbbe99363f9a167a9d601595f
Minsung Kim <minsungk101@gmail.com>
2025-12-24 21:09:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   console.log(userChosenColour)
-
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
@@ -21,6 +21,11 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+
 }
